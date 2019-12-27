a0dba12cc1a47cfeb9ba72124e3f3335c0148ba5
Suong Sambo <soungsombo@cubetiqs.com>
2019-12-27 13:20:43 +0700

    laravel

---
+++ resources/views/pages/service.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Service Page PHP</title>
+</head>
+
+<body>
+    <h1>Laravel Service Page</h1>
+    <p>Lorem ipsum dolor sit amet.</p>
+    <p>Lorem ipsum dolor sit amet.</p>
+    <p>Lorem ipsum dolor sit amet.</p>
+    <p>Lorem ipsum dolor sit amet.</p>
+
+</body>
+
+</html>
